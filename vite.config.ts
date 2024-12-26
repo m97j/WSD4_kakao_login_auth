cc85bbe6b2d9680a58b4d918ac717c29515c6d2e
m97j <mmnkjiae@gmail.com>
2024-12-26 22:48:34 +0900

    edit6

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/wsd_sj4/', // GitHub Pages 배포용 base 경로 설정
+  base: '/',  // Netlify에서는 base 경로를 루트('/')로 설정
+  build: {
+    outDir: 'dist',  // 빌드 결과물 출력 디렉토리
+    rollupOptions: {
+      input: 'src/main.ts',  // main.ts를 엔트리 포인트로 설정
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
