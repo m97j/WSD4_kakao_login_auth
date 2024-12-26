2d1ad4b68b66e05edb9a01bfc2d7b0bb2e3aac66
m97j <mmnkjiae@gmail.com>
2024-12-26 23:22:58 +0900

    final edit4

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/wsd_sj4/', // GitHub Pages 배포용 base 경로 설정
