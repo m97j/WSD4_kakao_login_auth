aed64f0428988dab602e1cb77d7d0663754db823
m97j <mmnkjiae@gmail.com>
2025-07-21 23:26:55 +0900

    Update vite.config.ts: rename of repo base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/wsd_sj4/', // GitHub Pages 배포용 base 경로 설정
+  base: '/WSD4_kakao_login_auth/', // GitHub Pages 배포용 base 경로 설정
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
